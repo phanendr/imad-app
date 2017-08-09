bae859d4cf6afb88ed5473b4ef940a24b3885dca
phanendr <30836344+phanendr@users.noreply.github.com>
2017-08-09 20:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/articl-one.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articl-one.html'));
-});
-app.get('/ui/articl-two.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articl-two.html'));
-});
-app.get('/ui/articl-three.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articl-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
