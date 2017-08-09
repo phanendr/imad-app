e3ed0d6b505e45e0fe56e5f9794d5884eda456ea
phanendr <30836344+phanendr@users.noreply.github.com>
2017-08-09 20:41:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/articl-one.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articl-one.html'));
+});
+app.get('/ui/articl-two.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articl-two.html'));
+});
+app.get('/ui/articl-three.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articl-three.html'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
